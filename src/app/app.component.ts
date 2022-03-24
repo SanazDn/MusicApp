fa221c5b138f32b8ff96cd00d1a07f9679f4786e
Sanaz Dehghannayyeri <sanaz142@yahoo.com>
2022-03-23 23:06:27 -0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { User } from './User';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+
+export class AppComponent implements OnInit {
+  title = 'web422-a6';
+  searchString: string;
+  token: User;
+
+  constructor(private router: Router, private auth: AuthService) {}
+
+  handleSearch(): void {
+    this.router.navigate(['/search'], {
+      queryParams: { q: this.searchString },
+    });
+  }
+
+  ngOnInit(): void {
+    this.searchString = '';
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.token = this.auth.readToken();
+      }
+    });
+  }
+
+  logout(): void {
+    localStorage.clear();
+    this.router.navigate(['login']);
+  }
+}
